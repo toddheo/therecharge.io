5ff481ae950ba942a352d6f9554af7e3b84e205e
KimJaekyoon <kjk704@gmail.com>
2021-08-03 17:12:46 +0900

    fixed translation font error

--- src/Component/Desktop/About/index.js
+++ src/Component/Desktop/About/index.js
@@ -221,7 +221,7 @@ function About({ t }) {
                     style={{ height: "78px", width: "65.2px" }}
                   />
                 </div>
-                <div className="desc text Roboto_16pt_Regular">
+                <div className={"desc text " + (t("About/Recharge-is-on/5")[0] == "P" ? "Roboto_16pt_Regular" : "Roboto_20pt_Regular")}>
                   {t("About/Recharge-is-on/5")}
                 </div>
               </div>
